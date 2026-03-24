efd4939c3832a075ac8069f6f72dc0e6ea226d6d
uusun <yseon0311@naver.com>
2026-03-24 22:16:40 +0900

    init spring project

---
+++ src/main/java/com/example/practice/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.practice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
+
+@RestController
+public class HelloController {
+
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return helloService.getString();
+    }
+}
---
+++ src/main/java/com/example/practice/HelloService.java
@@ -0,0 +1,11 @@
+package com.example.practice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getString() {
+        return "hi String";
+    }
+}
